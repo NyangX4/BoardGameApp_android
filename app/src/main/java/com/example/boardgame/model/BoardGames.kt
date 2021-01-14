ef1b18a9754d7d4a3e024b41d3789d69ece63938
MunYeong0723 <ansdud0723@naver.com>
2021-01-14 18:21:09 +0900

    modified class BoardGames - add year variable

--- app/src/main/java/com/example/boardgame/model/BoardGames.kt
+++ app/src/main/java/com/example/boardgame/model/BoardGames.kt
@@ -4,6 +4,7 @@ class BoardGames {
     var id: Int? = 0
     var gameImage: Int? = 0
     var gameTitle: String? = null
+    var year : Int?  = 0
     var gameLevel: Float? = 0.0f
     var thumbCnt: Int? = 0
     var commentCnt: Int? = 0
@@ -16,11 +17,12 @@ class BoardGames {
     var rate: Float? = 0.0f
     var similarList: List<Int>? = null
 
-    constructor(id: Int?, gameImage: Int?, gameTitle: String?, gameLevel: Float?, thumbCnt: Int?, commentCnt: Int?,
+    constructor(id: Int?, gameImage: Int?, gameTitle: String?, year : Int?, gameLevel: Float?, thumbCnt: Int?, commentCnt: Int?,
                 themeList : List<String>?, people: String?, playTime: String?, age : String?, genre: String?, howToPlay: String?, rate: Float?, similarList: List<Int>?) {
         this.id = id
         this.gameImage = gameImage
         this.gameTitle = gameTitle
+        this.year = year
         this.gameLevel = gameLevel
         this.thumbCnt = thumbCnt
         this.commentCnt = commentCnt
@@ -34,6 +36,7 @@ class BoardGames {
         this.similarList = similarList
     }
 
+    fun yearToString() = year.toString() + "년"
     fun gameLevelToString() = gameLevel.toString() + " / 5"
     fun rateToString() = rate.toString() + " / 10"
 }
